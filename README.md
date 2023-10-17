dd04cf531e164cb8fac64397efb8400a898358ec
Sakib <sjsakib.bd@gmail.com>
2023-10-17 18:08:30 +0600

    added counter service with dockerfile

---
+++ README.md
@@ -0,0 +1,3 @@
+# A class infra
+
+An attempt to learn DevOps by building an _"A Class"_ infrastructure.
---
+++ counter/index.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+import http from 'http';
+
+const redisClient = createClient({ url: process.env.REDIS_URL });
+const COUNTER_KEY = 'a-class-infra:counter';
+const PORT = process.env.PORT || 3001;
+
+async function main() {
+  await redisClient.connect();
+
+  console.log('Connected to Redis');
+
+  http
+    .createServer(async (_req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+      const count = await redisClient.incr(COUNTER_KEY);
+
+      console.log(`Count: ${count}`);
+
+      throw new Error('Something went wrong');
+
+    })
+    .listen(PORT);
+
+    console.log(`Listening on port: ${PORT}`);
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
